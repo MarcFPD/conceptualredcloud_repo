52215b3c592f80d404ab39bef18c7e9c68c6e4a7
Marc <marcfduverneuil@gmail.com>
2022-12-22 17:21:08 -0500

    Addidng the first version of the files root and dev

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
+?>
+
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully to the server: $servername";
+?>
+<p>
+<a href="create_db.php">Create Database</a>
+</p>
+<p>
+<a href="/dev/index.php">Open Dev</a>
+</p>
